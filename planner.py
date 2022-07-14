54eb2fd09f819b8f2b7505c0a9ceefe23e3f31fa
MelissaZhu01 <70490416+MelissaZhu01@users.noreply.github.com>
2022-07-14 16:01:56 -0300

    Update planner.py

--- planner.py
+++ planner.py
@@ -111,7 +111,7 @@ class ProgressionPlanning(object):
         max_step = 10
         goal_state = self.problem.goal
         initialNode = Node(State(self.problem.init))
-        frontier = util.Frontier(lambda searchNode:(searchNode.g + W*searchNode.h))
+        frontier = Frontier(lambda searchNode:(searchNode.g + W*searchNode.h))
         frontier.push(initialNode)
         reached = False
         for i in range(max_step):
