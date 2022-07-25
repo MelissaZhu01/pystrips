c7e353c26e18f46bdf44d72bcd8ee307a46d5fec
MelissaZhu01 <70490416+MelissaZhu01@users.noreply.github.com>
2022-07-25 19:58:00 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,19 +1,3 @@
-echo '>> Solving pddl/satellite/problems/pfile06.pddl ...'
-python3 pystrips.py solve pddl/satellite/domain.pddl pddl/satellite/problems/pfile06.pddl --heuristics $1 --weight $2
-echo
-
-echo '>> Solving pddl/satellite/problems/pfile07.pddl ...'
-python3 pystrips.py solve pddl/satellite/domain.pddl pddl/satellite/problems/pfile07.pddl --heuristics $1 --weight $2
-echo
-
-echo '>> Solving pddl/satellite/problems/pfile08.pddl ...'
-python3 pystrips.py solve pddl/satellite/domain.pddl pddl/satellite/problems/pfile08.pddl --heuristics $1 --weight $2
-echo
-
-echo '>> Solving pddl/satellite/problems/pfile10.pddl ...'
-python3 pystrips.py solve pddl/satellite/domain.pddl pddl/satellite/problems/pfile10.pddl --heuristics $1 --weight $2
-echo
-
 echo '>> Solving pddl/satellite/problems/pfile09.pddl ...'
 python3 pystrips.py solve pddl/satellite/domain.pddl pddl/satellite/problems/pfile09.pddl --heuristics $1 --weight $2
 echo
