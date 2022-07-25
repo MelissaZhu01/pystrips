c186a278a9c489d892791bafccc053b9aaae7218
MelissaZhu01 <70490416+MelissaZhu01@users.noreply.github.com>
2022-07-25 15:48:07 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,4 +1,20 @@
 echo
-echo '>> Solving blocksworld/problems/probBLOCKS-04-0.pddl ...'
+echo '>> Solving pddl/satellite/problems/pfile01.pddl ...'
+python pystrips.py solve pddl/satellite/domain.pddl pddl/satellite/problems/pfile01.pddl --heuristics $1 --weight $2
 echo
-python pystrips.py solve pddl/blocksworld/domain.pddl pddl/blocksworld/problems/probBLOCKS-04-0.pddl --heuristics $1 --weight $2
+
+echo '>> Solving pddl/satellite/problems/pfile02.pddl ...'
+python pystrips.py solve pddl/satellite/domain.pddl pddl/satellite/problems/pfile02.pddl --heuristics $1 --weight $2
+echo
+
+echo '>> Solving pddl/satellite/problems/pfile03.pddl ...'
+python pystrips.py solve pddl/satellite/domain.pddl pddl/satellite/problems/pfile03.pddl --heuristics $1 --weight $2
+echo
+
+echo '>> Solving pddl/satellite/problems/pfile04.pddl ...'
+python pystrips.py solve pddl/satellite/domain.pddl pddl/satellite/problems/pfile04.pddl --heuristics $1 --weight $2
+echo
+
+echo '>> Solving pddl/satellite/problems/pfile05.pddl ...'
+python pystrips.py solve pddl/satellite/domain.pddl pddl/satellite/problems/pfile05.pddl --heuristics $1 --weight $2
+echo
