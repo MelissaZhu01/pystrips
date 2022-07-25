567df4bf50c587f58a03401fc076bacdb29b9204
MelissaZhu01 <70490416+MelissaZhu01@users.noreply.github.com>
2022-07-24 22:38:19 -0300

    Update planner.py

--- planner.py
+++ planner.py
@@ -116,12 +116,14 @@ class ProgressionPlanning(object):
         reached = False
         i = 0
         while reached == False:
+            i=i+1
             print("Antes:")
             print(frontier)
-            print("node")
-            frontiertemp = frontier
-            tempnode = frontiertemp._queue.pop()
-            print(tempnode.state)
+            if i ==59:
+                print("node")
+                frontiertemp = frontier
+                tempnode = frontiertemp._queue.pop()
+                print(tempnode.state)
             sNode = frontier.pop()
             print("depois:")
             print(frontier)
